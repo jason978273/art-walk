f504c7108921d23b00e5e648104bdb692740c2b4
jason978273 <gao.haoy@northeastern.edu>
2025-07-13 12:56:27 -0700

    feat: update Footer component styling and layout

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,14 +4,26 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <footer className='bg-white text-gray-800 bottom-0 left-0 right-0 w-full py-6 z-50'>
+    <footer className='bg-white text-gray-800 border-t border-gray-200 mt-auto py-6'>
       <div className='max-w-7xl mx-auto px-4'>
-        <div className='flex flex-col md:flex-row items-center justify-between gap-4'>
+        <div className='flex flex-col md:flex-row items-center justify-between gap-6'>
           <div className='sponsors'>
-            <h4 className='font-semibold mb-2'>Our Partners and Sponsors</h4>
-            <div className='flex gap-4'>
-              <Image src="/images/westvan.png" alt="Sponsor 1" width={80} height={30} className='opacity-80 hover:opacity-100 transition-opacity' />
+            <h4 className='font-semibold mb-2 text-center md:text-left'>Our Partners and Sponsors</h4>
+            <div className='flex gap-4 justify-center md:justify-start'>
+              <Image 
+                src="/images/westvan.jpg" 
+                alt="West Vancouver" 
+                width={80} 
+                height={30} 
+                className='opacity-80 hover:opacity-100 transition-opacity' 
+              />
             </div>
+          </div>
+          
+          <div className='text-center md:text-right'>
+            <p className='text-sm text-gray-600'>
+              © {new Date().getFullYear()} West Vancouver Art Walk. All rights reserved.
+            </p>
           </div>
         </div>
       </div>
